05c77634a4c83835fa36eed91057f979888d98fd
sri-nagasai-siva-paladugu <srinagasaisiva.paladugu@student.allenisd.org>
2022-09-08 22:26:19 -0500

    Fixed the errors with brackets and syntax, added if loops for the user to enter new words, and created messages for the user

--- CS-II/Projects/main.swift
+++ CS-II/Projects/main.swift
@@ -1,7 +1,6 @@
 //code is NOT DONE, right now it can only sort 2 words, most of it is finished though, uses bubble sort we talked about
 
 //2 arrays of strings, listWords holds words that were typed, sortedWords is where the sorted words go
-
 var listWords: [String] = []
 var sortedWords: [String] = []
 
@@ -11,6 +10,13 @@ var moreWords = true
 
 //alphabetSort func is where the words from listWords get sorted, uses bubble sort and a swap function
 //swap function still needs to be separated from the alphabetSort func to make it nicer
+
+
+
+
+
+
+
 
 func alphabetSort() {
   print(listWords.count)
@@ -33,15 +39,40 @@ func alphabetSort() {
 
 //checks if you want to enter more words to be sorted, when you type "leave" it exits and runs alphabetSort
 
-while moreWords {
-  if let input = readLine() {
-    if input == "leave"
-      moreWords = false
-      break
-    }
-  
-    listWords.append(input)
+//Opening message
+
+print("Hello!")
+print("This program sorts the words you enter alphabetically")
+
+var requiredWords = 2
+print("Please enter a word below")
+if requiredWords > 0 {
+if let newWord = readLine() {
+    print("Please enter one more word")
+    let newWord2 = readLine() 
 }
+}
+print("If you want to enter more words, please enter each word on a new line. If at any time, you would like to stop entering new words please enter [leave] to exit the program.")
+
+
+while moreWords {
+
+    if let input = readLine() {
+      if input == "leave" {
+          moreWords = false
+        print  ("Thank you for your input. Your words will be shorted shortly.")
+       break
+      }
+    }
+}
+
+
+
+//-----------Need to find a way to add user the user input to the list because the input is currently a String.Type as it is assigned to readLine() --------------------
+// ------------- Also,  we have to make sure that the user has to enter two words. ----------------------------------------
+  
+//    listWords.append(input)
+
 
 alphabetSort()
 //listWords.sort()
